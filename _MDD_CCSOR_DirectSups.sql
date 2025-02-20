cd529add4fd2394077a80f9d139fdf7bce06c8f3
nmorris-mdd <145142138+nmorris-mdd@users.noreply.github.com>
2025-02-19 18:15:30 -0800

    updated

--- _MDD_CCSOR_DirectSups.sql
+++ _MDD_CCSOR_DirectSups.sql
@@ -1,3 +1,18 @@
+
+--Alter PROCEDURE [dbo].[_CCSOR_Productivity]
+
+--AS
+--BEGIN
+	
+	SET NOCOUNT ON;
+
+
+IF OBJECT_ID (N'dbo._MDD_CCSOR_DirectSups') IS NOT NULL
+DROP TABLE dbo._MDD_CCSOR_DirectSups
+
+  select 
+	es.emp_id
+	,e.first_name + ' ' + e.last_name [Supervisor]
   into _MDD_CCSOR_DirectSups
   from EmployeeSupervisor es
   inner join employees e on  es.supervisor_emp_id= e.emp_id
